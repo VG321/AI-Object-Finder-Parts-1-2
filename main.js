553e300e2b5d9515408a32b2d24481c1aff151ca
VG321 <83223230+VG321@users.noreply.github.com>
2022-09-04 10:44:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+status = "";
+
+function preload(){
+    video = createVideo('video.mp4');
+}
+
+function setup(){
+    canvas = createCanvas(350, 250);
+    canvas.center();
+    video.hide;
+}
+
+function draw(){
+    image(video, 0, 0, 350, 250);
+}
+
+function start(){
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects...";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded");
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
